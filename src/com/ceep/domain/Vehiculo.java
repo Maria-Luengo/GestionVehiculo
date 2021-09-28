1ea0cfa1a13b4e24f36d808a300e13c36434e789
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-09-28 11:40:16 +0200

    Update Vehiculo.java

--- src/com/ceep/domain/Vehiculo.java
+++ src/com/ceep/domain/Vehiculo.java
@@ -9,6 +9,17 @@ public class Vehiculo {
     private double tarifa;
     private boolean disponible;
 
+    //CONSTRUCTOR
+    public Vehiculo(String matricula, String marca, String modelo, String color, double tarifa, boolean disponible) {    
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        this.disponible = disponible;
+    }
+
+    //G&S
     public String getMatricula() {
         return matricula;
     }
@@ -40,4 +51,18 @@ public class Vehiculo {
     public void setDisponible(boolean disponible) {
         this.disponible = disponible;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(" matricula: ").append(matricula);
+        sb.append("\n marca: ").append(marca);
+        sb.append("\n modelo: ").append(modelo);
+        sb.append("\n color: ").append(color);
+        sb.append("\n tarifa: ").append(tarifa);
+        sb.append("\n disponible: ").append(disponible);
+        return sb.toString();
+    }
+    
+    
 }
