b5aef492daaf4b61f1613870177833011669bd11
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-09-28 11:40:18 +0200

    Update PruebaGestion.java

--- src/com/ceep/test/PruebaGestion.java
+++ src/com/ceep/test/PruebaGestion.java
@@ -5,7 +5,9 @@ import com.ceep.domain.Vehiculo;
 public class PruebaGestion {
     
     public static void main(String[] args) {
+        Vehiculo v1 = new Vehiculo("1111CCC","Seat", "Ibiza", "gris",1000, true);
         
+        System.out.println(v1);
     }
     
 }
