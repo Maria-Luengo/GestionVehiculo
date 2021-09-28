731c08b12fc06c4a74b268b9a4a43066881ac171
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-09-28 11:32:06 +0200

    Initial commit

---
+++ src/com/ceep/domain/Vehiculo.java
@@ -0,0 +1,43 @@
+
+package com.ceep.domain;
+
+public class Vehiculo {
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private double tarifa;
+    private boolean disponible;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+}
---
+++ src/com/ceep/test/PruebaGestion.java
@@ -0,0 +1,11 @@
+
+package com.ceep.test;
+import com.ceep.domain.Vehiculo;
+
+public class PruebaGestion {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
