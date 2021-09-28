04cb7b9e330d168f0dae2ac72ef8ac8bf1e075fe
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-09-28 12:29:57 +0200

    Update PruebaGestion.java

--- src/com/ceep/test/PruebaGestion.java
+++ src/com/ceep/test/PruebaGestion.java
@@ -2,6 +2,7 @@
 package com.ceep.test;
 import com.ceep.domain.Vehiculo;
 //import com.ceep.domain.*; //esto me importaría todo, es el comidin
+//Todas las clases son extens de la clase Object
 
 public class PruebaGestion {
     
@@ -9,12 +10,14 @@ public class PruebaGestion {
         Vehiculo v1 = new Vehiculo("1111CCC","Seat", "Ibiza", "gris",1000, true);
         Vehiculo v2 = new Vehiculo("2222LLL","Kia", "Ceep", "negro",5000, false);
         Vehiculo v3 = new Vehiculo();
+        //ctr+espacio -> saca los datos del constructor
         
         v3.setDisponible(false);
         
         System.out.println(v1);
         System.out.println(v2);
         System.out.println(v3);
+        System.out.println("v3 = " + v3);
     }
     
 }
