9fe157cedeb1a10b45b481480fb7608793e2ed15
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-09-28 11:53:20 +0200

    Update PruebaGestion.java

--- src/com/ceep/test/PruebaGestion.java
+++ src/com/ceep/test/PruebaGestion.java
@@ -6,8 +6,14 @@ public class PruebaGestion {
     
     public static void main(String[] args) {
         Vehiculo v1 = new Vehiculo("1111CCC","Seat", "Ibiza", "gris",1000, true);
+        Vehiculo v2 = new Vehiculo("2222LLL","Kia", "Ceep", "negro",5000, false);
+        Vehiculo v3 = new Vehiculo();
+        
+        v3.setDisponible(false);
         
         System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
     }
     
 }
